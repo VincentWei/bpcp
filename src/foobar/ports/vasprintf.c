d899688734065c33abba292b306cb5f4f4fb0dfa
Vincent Wei <vincent@minigui.org>
2021-09-18 18:40:19 +0800

    a bad header

--- src/foobar/ports/vasprintf.c
+++ src/foobar/ports/vasprintf.c
@@ -6,21 +6,6 @@
  *
  * Copyright (C) 2021 FMSoft <https://www.fmsoft.cn>
  *
- * This file is a part of PurC (short for Purring Cat), an HVML interpreter.
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
  * Note that the code is derived from json-c which is licensed under MIT Licence.
  *
  * Author: Michael Clark <michael@metaparadigm.com>
