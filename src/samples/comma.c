eb42c81135bfc45c36577f287732a31f866606d5
Vincent Wei <vincent@minigui.org>
2022-01-13 08:45:25 +0800

    tune:

--- src/samples/comma.c
+++ src/samples/comma.c
@@ -4,12 +4,20 @@
 int main(void)
 {
     int a, b, c, max;
+    int n;
 
     do {
         errno = 0;
         printf("Please input three integers (-2147483648～2147483647)\n");
-        scanf("%d,%d,%d", &a, &b, &c);
-    } while (errno == ERANGE);
+        n = scanf("%d%d%d", &a, &b, &c);
+
+        if (n != 3) {
+            int ch;
+            do {
+                ch = getchar();
+            } while (ch != '\n');
+        }
+    } while (errno == ERANGE || n != 3);
 
     if (a > b && a > c)
         max = a;
